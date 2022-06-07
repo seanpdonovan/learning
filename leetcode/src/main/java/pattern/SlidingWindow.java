59d5c8863d8fde3b8ee0d25074d820e728df25fc
Sean Donovan <seandonbu@gmail.com>
2022-06-07 09:07:01 -0500

    remove old projects

--- leetcode/src/main/java/pattern/SlidingWindow.java
+++
@@ -1,9 +0,0 @@
-package pattern;
-
-public interface SlidingWindow {
-    int MinimumSizeSubarraySum(int target, int[] nums);
-
-    int FruitsIntoBaskets(int[] fruits);
-
-    boolean PermutationInString(String s1, String s2);
-}
